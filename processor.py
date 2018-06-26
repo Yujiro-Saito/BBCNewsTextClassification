d1c61d2591e3d39fca7fd97e2a8d9d6558d66e17
Yujiro Saito <>
2018-06-26 22:22:41 +0900

    ニュースセット学習用に整形

--- processor.py
+++ processor.py
@@ -4,23 +4,23 @@ import codecs
 import io,sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-directory = os.listdir("./tech")
+directory = os.listdir("./sport")
 array = []
 
 for file in directory:
-    f = codecs.open("./tech/{}".format(file),'r', 'utf-8', errors='ignore')
+    f = codecs.open("./sport/{}".format(file),'r', 'utf-8', errors='ignore')
     lines = f.read()
     fixedLines = lines.split("\n")
     non_spaceList = [w for w in fixedLines if not w in '']
     #Add "." to the title.
     non_spaceList.pop(0)
     nonSpaceString = " ".join(non_spaceList)
-    labelStr = "__label__tech "
+    labelStr = "__label__sport "
     processedSent = labelStr + nonSpaceString
     array.append(processedSent)
 
 correctString = "\n".join(array)
 
-with open("demo.txt", "w", encoding='utf-8') as text_file:
+with open("sportNews.txt", "w", encoding='utf-8') as text_file:
     text_file.write(correctString)
 
