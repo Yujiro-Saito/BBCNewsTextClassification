eaef3d765601ec27d1baff9a23743ab7e61755a8
Yujiro Saito <>
2018-06-26 22:16:57 +0900

    Text-preprocess

--- processor.py
+++ processor.py
@@ -10,9 +10,17 @@ array = []
 for file in directory:
     f = codecs.open("./tech/{}".format(file),'r', 'utf-8', errors='ignore')
     lines = f.read()
-    test = lines.split("\n")
-    non_spaceList = [w for w in test if not w in '']
+    fixedLines = lines.split("\n")
+    non_spaceList = [w for w in fixedLines if not w in '']
     #Add "." to the title.
     non_spaceList.pop(0)
     nonSpaceString = " ".join(non_spaceList)
-    array.append(nonSpaceString)
+    labelStr = "__label__tech "
+    processedSent = labelStr + nonSpaceString
+    array.append(processedSent)
+
+correctString = "\n".join(array)
+
+with open("demo.txt", "w", encoding='utf-8') as text_file:
+    text_file.write(correctString)
+
